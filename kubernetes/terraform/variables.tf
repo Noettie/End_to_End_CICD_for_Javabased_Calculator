859c27d07ffbfcca33b0a6b50981667bb80a20c8
Noettie <dev.ndlovun@gmail.com>
2025-06-12 08:11:27 +0000

    kops cluster files

---
+++ kubernetes/terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for KOPS state"
+  type        = string
+  default     = "petra-kops-state-bucket-12345" # Must be globally unique!
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  description = "Name of Kubernetes cluster"
+  type        = string
+  default     = "petra-cluster.k8s.local"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair to use for EC2 instances"
+  type        = string
+  default     = "petra-hs-project"
+}
+
